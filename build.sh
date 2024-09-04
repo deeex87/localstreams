4117fa80bb50df80cb5b4380b6dd8241ebe1b98f
Fran Lerma <deeex87@users.noreply.github.com>
2024-09-04 13:15:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker build . -t deeex87/localstream || exit 1
-#docker run -it --name localstreams --publish 15123:15123 -l com.centurylinklabs.watchtower.enable=false -l wud.watch=false --restart always  deeex87/localstreams
+PROJECT_NAME="localstreams"
+IMAGE_NAME="franlerma/$PROJECT_NAME"
+docker build . -t $IMAGE_NAME || exit 1
+#docker run -it --name $PROJECT_NAME --publish 15123:15123 -l com.centurylinklabs.watchtower.enable=false -l wud.watch=false --restart always  $IMAGE_NAME
