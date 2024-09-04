b216f165074816497bd1e8e220374e4b5446928e
Fran Lerma <deeex87@users.noreply.github.com>
2024-09-04 13:34:13 +0200

    Añadida cache acestream

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 PROJECT_NAME="localstreams"
 IMAGE_NAME="franlerma/$PROJECT_NAME"
 docker build . -t $IMAGE_NAME || exit 1
-#docker run -it --name $PROJECT_NAME --publish 15123:15123 -l com.centurylinklabs.watchtower.enable=false -l wud.watch=false --restart always  $IMAGE_NAME
+docker run -it --name $PROJECT_NAME --publish 15123:15123 -l com.centurylinklabs.watchtower.enable=false -l wud.watch=false --restart always  $IMAGE_NAME
