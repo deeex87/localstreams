95c8dc7bda3299ef18e2c5e36e5eb381773624e7
Fran Lerma <deeex87@users.noreply.github.com>
2024-09-28 18:15:43 +0200

    Cambios import

--- app/acestream/acestream_arm.py
+++ app/acestream/acestream_arm.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 ACEADDON="/opt/acestream"
 ACECHROOT="androidfs"
@@ -9,4 +10,7 @@ def launch_acestream_arm(acestream_args, acestream_port, acestream_cache_dir):
     #chroot $ACEADDON/$ACECHROOT /system/bin/busybox sh -c "/system/bin/acestream.sh" > $ACEADDON/acestream.log 2>&1
     command = ['chroot', f"{ACEADDON}/{ACECHROOT}", '/system/bin/busybox', 'sh', '-c', f"/system/bin/acestream.sh > {ACEADDON}/acestream.log 2>&1"]
 
-    return command
+    acestream_process = subprocess.Popen(command, 
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    return acestream_process
