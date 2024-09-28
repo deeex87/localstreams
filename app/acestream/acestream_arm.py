3e834454eac45a8fd82a747f608735767298f766
Fran Lerma <deeex87@users.noreply.github.com>
2024-09-28 18:03:45 +0200

    Update acestream_arm.py

--- app/acestream/acestream_arm.py
+++ app/acestream/acestream_arm.py
@@ -1,9 +1,12 @@
 import os
 
+ACEADDON="/opt/acestream"
+ACECHROOT="androidfs"
+
+
 def launch_acestream_arm(acestream_args, acestream_port, acestream_cache_dir):
     ACESTREAM_BINARY = os.getenv("ACESTREAM_BINARY", "/opt/acestream/acestreamengine-arm64") + "/acestreamengine"
     #chroot $ACEADDON/$ACECHROOT /system/bin/busybox sh -c "/system/bin/acestream.sh" > $ACEADDON/acestream.log 2>&1
-    command = [ ACESTREAM_BINARY, "--client-console", "--http-port", f"{acestream_port}", 
-                    "--cache-dir", f"{acestream_cache_dir}", #"--cache-limit", f"{ACESTREAM_CACHE_LIMIT}", 
-                    "", "--bind-all", acestream_args]
+    command = ['chroot', f"{ACEADDON}/{ACECHROOT}", '/system/bin/busybox', 'sh', '-c', f"/system/bin/acestream.sh > {ACEADDON}/acestream.log 2>&1"]
+
     return command
